7f955fe967bda1b37b978075ed420a94fd4657ff
Nickolas Kenyeres <info@knickmedia.com>
2011-12-07 20:57:41 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macchiato::Application.config.secret_token = 'e66ffef11759cbdbcb001e6ba1cd5fe10d166298702de59722a513809c8877e59d880a51c6033b4b9abcdd7c88228d38e7ea3a3a55a68ba42bdf763847a89930'
